50522a81f048a7d47f97cf75a424409ed01ea820
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-09-29 21:17:32 +0000

    Completed

--- Main.java
+++ Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
 public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Give me a word!");
+      String word = scan.next();
+
+      System.out.println("Give me another word!");
+      String word2 = scan.next();
+
+      System.out.printf("Now give me a number!%n");
+      int num = scan.nextInt();
+
+      System.out.printf("Output:%n" + lastFirstN(word, word2, num) + "%n%n");
+
+      System.out.println("This time, give me a word to put in all uppercase!");
+      word = scan.next();
+
+      System.out.printf("Now give me another word to make the first letter uppercase!%n");
+      word2 = scan.next();
+
+      StringManip result = new StringManip(word, word2);
+      System.out.printf("%nOutput:%n%s%n%s%n",result.getStr1(), result.getStr2());
+  }
+
+  public static String lastFirstN(String s1, String s2, int n)
+  {
+      String finalAnswer;
+
+      finalAnswer = s1.substring((s1.length() - n), s1.length());
+      finalAnswer += s2.substring(0, n);
+
+      return finalAnswer;
   }
 }
---
+++ StringManip.java
@@ -0,0 +1,39 @@
+public class StringManip
+{
+  private final String allCAPS;
+  private final String oneCAP;
+
+  public StringManip(String str1, String str2)
+  {
+    String allCAPS;
+    String oneCAP = "";
+
+    allCAPS = str1.toUpperCase();
+
+    for(int i = 0; i < str2.length(); i++)
+    {
+      if(i == 0)
+      {
+        oneCAP = str2.substring(0,1).toUpperCase();
+      }
+      else
+      {
+        oneCAP += str2.substring(i,i+1).toLowerCase();
+      }
+    }
+
+    this.allCAPS = allCAPS;
+    this.oneCAP = oneCAP;
+  }
+
+  public String getStr1() 
+  {
+    return allCAPS;
+  }
+
+  public String getStr2()
+  {
+    return oneCAP;
+  }
+
+}
